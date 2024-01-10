a1b366fbb254f7cc694d782bf40ba3ae7fdee338
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-10 18:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import plotly.express as px
 
-data = [0.5, 0.3, 0.4, 0.9, 0.9]
+data = [
+    [0.5, 0.3, 0.4, 0.9, 0.9],
+    [0.9, 0.1, 0.7, 0.4, 0.8],
+]
 
 # Create labels for the radar plot
 labels = ['Hy', 'CTF', 'CFA', 'TFA', 'BD']
 
-# Repeat the labels for each set of values
-theta = labels * len(data)
+# Flatten the data for plotting
+flat_data = [value for sublist in data for value in sublist]
 
-# Create a polar line plot
+# Repeat the labels for each set of values
+labels *= len(data)
+
+# Repeat the labels for each set of values
 fig = px.line_polar(
-    r=data * len(labels),  # Repeat the data for each label
-    theta=theta,
+    r=flat_data,
+    theta=labels,
     line_close=True,
-    range_r=[0, 1.0],  # Adjust the range based on your data
+    range_r=[0, 1.0],
     title="Your Radar Plot Title"
 )
 
