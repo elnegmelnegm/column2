adfc5fde5dc96a49353f6797cf3c1eeb565de650
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-10 19:10:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
+import streamlit as st
 import plotly.express as px
 
-data = [
-    [0.5, 0.3, 0.4, 0.9, 0.9],
-    [0.9, 0.1, 0.7, 0.4, 0.8],
-]
+def main():
+    st.title("Streamlit Radar Plot")
 
-# Create labels for the radar plot
-labels = ['Hy', 'CTF', 'CFA', 'TFA', 'BD']
+    data = [
+        [0.5, 0.3, 0.4, 0.9, 0.9],
+        [0.9, 0.1, 0.7, 0.4, 0.8],
+    ]
 
-# Flatten the data for plotting
-flat_data = [value for sublist in data for value in sublist]
+    labels = ['Hy', 'CTF', 'CFA', 'TFA', 'BD']
+    flat_data = [value for sublist in data for value in sublist]
+    labels *= len(data)
 
-# Repeat the labels for each set of values
-labels *= len(data)
+    fig = px.line_polar(
+        r=flat_data,
+        theta=labels,
+        line_close=True,
+        range_r=[0, 1.0],
+        title="Your Radar Plot Title"
+    )
 
-# Repeat the labels for each set of values
-fig = px.line_polar(
-    r=flat_data,
-    theta=labels,
-    line_close=True,
-    range_r=[0, 1.0],
-    title="Your Radar Plot Title"
-)
+    st.plotly_chart(fig)
 
-fig.show()
+if __name__ == '__main__':
+    main()
