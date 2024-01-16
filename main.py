20e8ef3088cea7d6a918e4ddc750664a1c74fdd9
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-16 11:24:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.image(image_path)
 def main():
     st.title("Columns Radar Plot")
     st.subheader("A web application that allows you to quickly choose equivalent columns for HPLC")
- st.markdown('Check out [Hydrophobicity (Hy)](https://apps.usp.org/app/USPNF/columnsGlossary.html#HY), [Chelating Tailing Factor (CTF)](https://apps.usp.org/app/USPNF/columnsGlossary.html#CTF), [Capacity Factor Amitriptyline (CFA)](https://apps.usp.org/app/USPNF/columnsGlossary.html#CFA), [Tailing Factor of Amitriptyline (TFA)](https://apps.usp.org/app/USPNF/columnsGlossary.html#TFA), [Bonding density (BD)](https://apps.usp.org/app/USPNF/columnsGlossary.html#BD)') 
+st.markdown('Check out [Hydrophobicity (Hy)](https://apps.usp.org/app/USPNF/columnsGlossary.html#HY), [Chelating Tailing Factor (CTF)](https://apps.usp.org/app/USPNF/columnsGlossary.html#CTF), [Capacity Factor Amitriptyline (CFA)](https://apps.usp.org/app/USPNF/columnsGlossary.html#CFA), [Tailing Factor of Amitriptyline (TFA)](https://apps.usp.org/app/USPNF/columnsGlossary.html#TFA), [Bonding density (BD)](https://apps.usp.org/app/USPNF/columnsGlossary.html#BD)') 
 
     
 
